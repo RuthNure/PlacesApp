23d83bc5ddaf4830f60a285e75ef446dd53b630a
Ruth Nure <rnure1@student.gsu.edu>
2025-04-27 23:36:26 -0400

    Initial Commit

---
+++ PlacesAppTests/PlacesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesAppTests.swift
+//  PlacesAppTests
+//
+//  Created by Ruth on 4/27/25.
+//
+
+import Testing
+@testable import PlacesApp
+
+struct PlacesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
